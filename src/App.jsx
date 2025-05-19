7b915058cffa6339b4b7818fa469dc7df7658727
EA - Lavinia Reis Conceição <lavinia_reis@educadventista.org>
2025-05-19 15:07:53 -0300

    acho q foi

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
 import { useState } from "react"
 
 export function App() {
-  const [numero, setNumero] = useState(0)
+  const [Esconder, setEsconder] = useState (false)
+  const [numero, setNumero] = useState(100)
 
   function handleAumentar(){
-    setNumero(numero + 1)
+    setNumero(numero + 100)
+    console.log(numero)
   }
+
+  function handleMostrarEsconder (){
+    Esconder === true ? setEsconder(false) : setEsconder(true);
+  }
+
   return(
     <section>
       <h1>App</h1>
 
       <div>
         <h2>numero:{numero}</h2>
+
         <button onClick={handleAumentar}>Aumentar</button>
+        
+
+        <button onClick={handleMostrarEsconder}>{Esconder === true ? "Esconder texto" : "Mostrar texto"}</button>
+        {Esconder === true ? <p>Mostrar Texto</p> : "" }
+
       </div>
     </section>
+
   )
 }
 
