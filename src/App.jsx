0ecc2f1f9511c33ad2243e30eb9bb0ebc7448ae1
EA - Lavinia Reis Conceição <lavinia_reis@educadventista.org>
2025-05-19 14:53:13 -0300

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-export function App() {
-  let num = 10
+import { useState } from "react"
 
+export function App() {
+  const [numero, setNumero] = useState(0)
+
+  function handleAumentar(){
+    setNumero(numero + 1)
+  }
   return(
     <section>
       <h1>App</h1>
 
       <div>
-        <h2>numero: {num}</h2>
+        <h2>numero:{numero}</h2>
+        <button onClick={handleAumentar}>Aumentar</button>
       </div>
     </section>
   )
